2a2499c2548eb8c4a55d0dd1eca651de9db73368
TomasLasca <tomaslasca99@gmail.com>
2022-11-14 11:46:16 -0300

    agrego clases de pedido

---
+++ src/main/java/MVC/DAO/PedidoDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MVC.DAO;
+
+import MVC.Models.Pedido;
+
+/**
+ *
+ * @author Tomas
+ */
+public class PedidoDAO {
+    private Pedido pedido;
+    
+    public void cargarPedido(Pedido pedido){
+        
+    }
+    
+    public void verPedidos(){
+        
+    }
+    
+}
